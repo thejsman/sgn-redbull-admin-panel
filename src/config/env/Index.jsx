5c2e91a81d1f40e6fabdd6d750a3cd508e570275
shipra <shipra.agrawal@sagoon.com>
2022-06-08 15:11:39 +0530

    update url

--- src/config/env/Index.jsx
+++ src/config/env/Index.jsx
@@ -1,9 +1,32 @@
-let siteSetting = {
+
+import _ from 'lodash';
+import { devSetting, stagSetting, prodSetting, localhostSetting } from './Setting';
+
+let defaultSetting = {
     api: {
         BaseUrl: process.env.REACT_APP_API_BASE_URL,
         mode: 'cors',
-        WebSocketUrl: process.env.REACT_APP_WEB_SOCKET_URL
+        WebSocketUrl: process.env.REACT_APP_WEB_SOCKET_URL,
     }
+
+}
+let siteSetting = defaultSetting;
+
+switch (process.env.REACT_APP_ENV) {
+    case "prod":
+    case "production":
+        siteSetting = _.extend(defaultSetting, prodSetting);
+        break;
+    case "stag":
+    case "staging":
+        siteSetting = _.extend(defaultSetting, stagSetting);
+        break;
+    case "local":
+    case "localhost":
+        siteSetting = _.extend(defaultSetting, localhostSetting);
+        break;
+    default:
+        siteSetting = _.extend(defaultSetting, devSetting);
 }
 console.log('env', process.env);
 
