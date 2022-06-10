bab3565b135dfe5efed547b5222c5b8630f224a1
shipra <shipra.agrawal@sagoon.com>
2022-06-10 11:34:06 +0530

    update occasion api  url

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -4,6 +4,7 @@ import { Bar } from 'react-chartjs-2';
 import { Stats } from '../../components/common/Stats';
 import { CreditDebitStats } from '../../components/common/CreditDebitStats';
 import siteSetting from "../../config/env/Index";
+const URL = siteSetting.api.WebSocketUrl;
 
 
 const Dashboard = () => {
@@ -20,6 +21,84 @@ const Dashboard = () => {
 
   const [tolDebitAmount, setTolDebitAmount] = useState([0]);
   const [tolMonthDebitAmount, setTolMonthDebitAmount] = useState([0]);
+
+
+  const clientRef = useRef(null);
+  const [waitingToReconnect, setWaitingToReconnect] = useState(null);
+  const [messages, setMessages] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+
+  // useEffect(() => {
+
+  //   if (waitingToReconnect) {
+  //     return;
+  //   }
+
+  //   // Only set up the websocket once
+  //   if (!clientRef.current) {
+  //     const client = new WebSocket(URL);
+  //     clientRef.current = client;
+
+  //     window.client = client;
+
+  //     client.onerror = (e) => console.error(e);
+
+  //     client.onopen = () => {
+  //       setIsOpen(true);
+  //       console.log('ws opened');
+  //       client.send('ping');
+  //     };
+
+  //     client.onclose = () => {
+  //       debugger;
+  //       console.log('ws closed by server');
+  //       if (clientRef.current) {
+  //         // Connection failed
+  //         console.log('ws closed by server');
+  //       } else {
+  //         // Cleanup initiated from app side, can return here, to not attempt a reconnect
+  //         console.log('ws closed by app component unmount');
+  //         return;
+  //       }
+
+  //       if (waitingToReconnect) {
+  //         return;
+  //       };
+
+  //       // Parse event code and log
+  //       setIsOpen(false);
+  //       console.log('ws closed');
+
+  //       // Setting this will trigger a re-run of the effect,
+  //       // cleaning up the current websocket, but not setting
+  //       // up a new one right away
+  //       setWaitingToReconnect(true);
+
+  //       // This will trigger another re-run, and because it is false,
+  //       // the socket will be set up again
+  //       setTimeout(() => setWaitingToReconnect(null), 5000);
+  //     };
+
+
+  //     client.onmessage = message => {
+  //       console.log('received message', message);
+  //       addMessage(`received '${message.data}'`);
+  //     };
+
+
+  //     return () => {
+
+  //       console.log('Cleanup');
+  //       // Dereference, so it will set up next time
+  //       clientRef.current = null;
+
+  //       client.close();
+  //     }
+  //   }
+
+  // }, [waitingToReconnect]);
+
+
 
 
   useEffect(() => {
@@ -127,11 +206,20 @@ const Dashboard = () => {
 
   }, []);
 
+  function addMessage(message) {
+    setMessages([...messages, message]);
+  }
 
 
 
   return (
+
     <div className='page_wrapper'>
+      {/* <div>
+        <h1>Websocket {isOpen ? 'Connected' : 'Disconnected'}</h1>
+        {waitingToReconnect && <p>Reconnecting momentarily...</p>}
+        {messages.map(m => <p>{JSON.stringify(m, null, 2)}</p>)}
+      </div> */}
       <div className="row">
 
         <Stats {...userData} arrUser={arrUser} arrMonthUser={arrMonthUser} ></Stats>
