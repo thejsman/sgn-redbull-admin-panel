ca43967e28337bce4c847558a8694cce5ef2e515
shipra <shipra.agrawal@sagoon.com>
2022-06-10 16:31:42 +0530

    add reconnection websocket functionality on dashboard page

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -27,199 +27,179 @@ const Dashboard = () => {
   const [waitingToReconnect, setWaitingToReconnect] = useState(null);
   const [messages, setMessages] = useState([]);
   const [isOpen, setIsOpen] = useState(false);
-
-  // useEffect(() => {
-
-  //   if (waitingToReconnect) {
-  //     return;
-  //   }
-
-  //   // Only set up the websocket once
-  //   if (!clientRef.current) {
-  //     const client = new WebSocket(URL);
-  //     clientRef.current = client;
-
-  //     window.client = client;
-
-  //     client.onerror = (e) => console.error(e);
-
-  //     client.onopen = () => {
-  //       setIsOpen(true);
-  //       console.log('ws opened');
-  //       client.send('ping');
-  //     };
-
-  //     client.onclose = () => {
-  //       debugger;
-  //       console.log('ws closed by server');
-  //       if (clientRef.current) {
-  //         // Connection failed
-  //         console.log('ws closed by server');
-  //       } else {
-  //         // Cleanup initiated from app side, can return here, to not attempt a reconnect
-  //         console.log('ws closed by app component unmount');
-  //         return;
-  //       }
-
-  //       if (waitingToReconnect) {
-  //         return;
-  //       };
-
-  //       // Parse event code and log
-  //       setIsOpen(false);
-  //       console.log('ws closed');
-
-  //       // Setting this will trigger a re-run of the effect,
-  //       // cleaning up the current websocket, but not setting
-  //       // up a new one right away
-  //       setWaitingToReconnect(true);
-
-  //       // This will trigger another re-run, and because it is false,
-  //       // the socket will be set up again
-  //       setTimeout(() => setWaitingToReconnect(null), 5000);
-  //     };
-
-
-  //     client.onmessage = message => {
-  //       console.log('received message', message);
-  //       addMessage(`received '${message.data}'`);
-  //     };
-
-
-  //     return () => {
-
-  //       console.log('Cleanup');
-  //       // Dereference, so it will set up next time
-  //       clientRef.current = null;
-
-  //       client.close();
-  //     }
-  //   }
-
-  // }, [waitingToReconnect]);
-
-
-
+  const URL = siteSetting.api.WebSocketUrl;
 
   useEffect(() => {
-    //if (isMounted.current) {
-    const ws = new WebSocket(siteSetting.api.WebSocketUrl);
 
-    ws.onopen = () => {
-      console.log("connection open");
-      ws.send(JSON.stringify({}));
-    };
-    ws.onmessage = (event) => {
-      let response = JSON.parse(event.data);
-      if (response.userStats) {
-        console.log('Receive Data', (response.userStats))
-        let dates = response.userStats.analysis.map(function (i) {
-          return i.date;
-        });
-        let userCounts = response.userStats.analysis.map(function (i) {
-          return i.userCount;
-        });
-        let arr = arrUser;
-        arr.push(response.userStats["totalUsers"]);
-        setArrUser([...arr]);
+    if (waitingToReconnect) {
+      return;
+    }
 
-        let arrMonth = arrMonthUser;
-        arrMonth.push(response.userStats["monthUser"]);
-        setArrMonthUser([...arrMonth]);
+    // Only set up the websocket once
+    if (!clientRef.current) {
+      const client = new WebSocket(URL);
+      clientRef.current = client;
 
-        let userObj = {
-          totalUsers: response.userStats["totalUsers"],
-          monthUser: response.userStats["monthUser"],
-          chartDetail: { labels: dates, data: userCounts }
+      window.client = client;
+
+      client.onerror = (e) => console.error(e);
+
+      client.onopen = () => {
+        setIsOpen(true);
+        console.log('ws opened');
+        client.send('ping');
+      };
+
+      client.onclose = () => {
+        debugger;
+        console.log('ws closed by server');
+        if (clientRef.current) {
+          // Connection failed
+          console.log('ws closed by server');
+        } else {
+          // Cleanup initiated from app side, can return here, to not attempt a reconnect
+          console.log('ws closed by app component unmount');
+          return;
         }
 
-        setUserData(userObj);
+        if (waitingToReconnect) {
+          return;
+        };
 
-      }
-      if (response.creditStats) {
+        // Parse event code and log
+        setIsOpen(false);
+        console.log('ws closed');
 
-        let dates = response.creditStats.analysis.map(function (i) {
-          return i.date;
-        });
-        let amount = response.creditStats.analysis.map(function (i) {
-          return i.amount;
-        });
-        let principalAmt = parseInt(response.creditStats["principalAmount"]) - (response.creditStats["totalAmount"]);
+        // Setting this will trigger a re-run of the effect,
+        // cleaning up the current websocket, but not setting
+        // up a new one right away
+        setWaitingToReconnect(true);
 
-        let arrPriAmt = principalAmount;
-        arrPriAmt.push(principalAmt);
-        setPrincipalAmount([...arrPriAmt]);
-
-        let arrtotAmt = tolCreditAmount;
-        arrtotAmt.push(response.creditStats["totalAmount"]);
-        setTolCreditAmount([...arrtotAmt]);
-
-        let arrtotMonthAmt = tolMonthCreditAmount;
-        arrtotMonthAmt.push(response.creditStats["monthAmount"]);
-        setTolMonthCreditAmount([...arrtotMonthAmt]);
+        // This will trigger another re-run, and because it is false,
+        // the socket will be set up again
+        setTimeout(() => setWaitingToReconnect(null), 5000);
+      };
 
 
-        let creditObj = {
-          principalAmount: principalAmt,
-          totalAmount: response.creditStats["totalAmount"],
-          monthAmount: response.creditStats["monthAmount"],
-          chartDetail: { labels: dates, data: amount }
-        }
-        setCreditData(creditObj);
-
-      }
-
-      if (response.debitStats) {
-
-        let dates = response.debitStats.analysis.map(function (i) {
-          return i.date;
-        });
-        let amount = response.debitStats.analysis.map(function (i) {
-          return i.amount;
-        });
+      client.onmessage = message => {
+        console.log('received message', message);
+        addMessage(message.data);
+      };
 
 
+      // return () => {
 
-        let arrtotDebAmt = tolDebitAmount;
-        arrtotDebAmt.push(response.debitStats["totalAmount"]);
-        setTolDebitAmount([...arrtotDebAmt]);
+      //   console.log('Cleanup');
+      //   // Dereference, so it will set up next time
+      //   clientRef.current = null;
 
-        let arrtotMonthDebAmt = tolMonthDebitAmount;
-        arrtotMonthDebAmt.push(response.debitStats["monthAmount"]);
-        setTolMonthDebitAmount([...arrtotMonthDebAmt]);
+      //   client.close();
+      // }
+    }
+
+  }, [waitingToReconnect]);
 
 
-        let debitObj = {
-          totalAmount: response.debitStats["totalAmount"],
-          monthAmount: response.debitStats["monthAmount"],
-          chartDetail: { labels: dates, data: amount }
-        }
-        setDebitData(debitObj);
-      }
-
-    };
-
-    // return () => {
-    //   isMounted.current = false;
-    // };
-    //}
-
-  }, []);
 
   function addMessage(message) {
-    setMessages([...messages, message]);
-  }
+    let response = JSON.parse(message);
+    if (response.userStats) {
+      console.log('Receive Data', (response.userStats))
+      let dates = response.userStats.analysis.map(function (i) {
+        return i.date;
+      });
+      let userCounts = response.userStats.analysis.map(function (i) {
+        return i.userCount;
+      });
+      let arr = arrUser;
+      arr.push(response.userStats["totalUsers"]);
+      setArrUser([...arr]);
+
+      let arrMonth = arrMonthUser;
+      arrMonth.push(response.userStats["monthUser"]);
+      setArrMonthUser([...arrMonth]);
+
+      let userObj = {
+        totalUsers: response.userStats["totalUsers"],
+        monthUser: response.userStats["monthUser"],
+        chartDetail: { labels: dates, data: userCounts }
+      }
+
+      setUserData(userObj);
+
+    }
+    if (response.creditStats) {
+
+      let dates = response.creditStats.analysis.map(function (i) {
+        return i.date;
+      });
+      let amount = response.creditStats.analysis.map(function (i) {
+        return i.amount;
+      });
+      let principalAmt = parseInt(response.creditStats["principalAmount"]) - (response.creditStats["totalAmount"]);
+
+      let arrPriAmt = principalAmount;
+      arrPriAmt.push(principalAmt);
+      setPrincipalAmount([...arrPriAmt]);
+
+      let arrtotAmt = tolCreditAmount;
+      arrtotAmt.push(response.creditStats["totalAmount"]);
+      setTolCreditAmount([...arrtotAmt]);
+
+      let arrtotMonthAmt = tolMonthCreditAmount;
+      arrtotMonthAmt.push(response.creditStats["monthAmount"]);
+      setTolMonthCreditAmount([...arrtotMonthAmt]);
+
+
+      let creditObj = {
+        principalAmount: principalAmt,
+        totalAmount: response.creditStats["totalAmount"],
+        monthAmount: response.creditStats["monthAmount"],
+        chartDetail: { labels: dates, data: amount }
+      }
+      setCreditData(creditObj);
+
+    }
+
+    if (response.debitStats) {
+
+      let dates = response.debitStats.analysis.map(function (i) {
+        return i.date;
+      });
+      let amount = response.debitStats.analysis.map(function (i) {
+        return i.amount;
+      });
+
+
+
+      let arrtotDebAmt = tolDebitAmount;
+      arrtotDebAmt.push(response.debitStats["totalAmount"]);
+      setTolDebitAmount([...arrtotDebAmt]);
+
+      let arrtotMonthDebAmt = tolMonthDebitAmount;
+      arrtotMonthDebAmt.push(response.debitStats["monthAmount"]);
+      setTolMonthDebitAmount([...arrtotMonthDebAmt]);
+
+
+      let debitObj = {
+        totalAmount: response.debitStats["totalAmount"],
+        monthAmount: response.debitStats["monthAmount"],
+        chartDetail: { labels: dates, data: amount }
+      }
+      setDebitData(debitObj);
+    }
+
+  };
+
+
+
 
 
 
   return (
 
     <div className='page_wrapper'>
-      {/* <div>
-        <h1>Websocket {isOpen ? 'Connected' : 'Disconnected'}</h1>
-        {waitingToReconnect && <p>Reconnecting momentarily...</p>}
-        {messages.map(m => <p>{JSON.stringify(m, null, 2)}</p>)}
-      </div> */}
+
       <div className="row">
 
         <Stats {...userData} arrUser={arrUser} arrMonthUser={arrMonthUser} ></Stats>
