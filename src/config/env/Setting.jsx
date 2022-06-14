dc22a23aaae73c065ac6b4a07b2bee029960dec8
shipra <shipra.agrawal@sagoon.com>
2022-06-14 10:46:02 +0530

    configure production url

--- src/config/env/Setting.jsx
+++ src/config/env/Setting.jsx
@@ -27,19 +27,19 @@ export const stagSetting = {
 //prod
 export const prodSetting = {
   api: {
-    url: "https://api.gosorted.com/Sagoon/api/v1/",
-    AUTH: "Basic c29ydGVkX0F1dGg6TXpDWTd5S0ZubHl4dTdHdg==",
-    mode: "cors",
-    REACT_APP_IMAGE_BASE_PATH: "https://dzem81w7r6ap7.cloudfront.net/",
-    s3_url: "https://cdn.gosorted.com/",
-    url_Prod: "https://nlpapi.gosorted.com/Sagoon/api/v1/",
+    BaseUrl: process.env.REACT_APP_API_BASE_URL,
+    mode: 'cors',
+    WebSocketUrl: process.env.REACT_APP_WEB_SOCKET_URL,
+    Occasion_url: process.env.REACT_APP_API_BASE_URL + "/admin-occasion",
   },
 };
 
 //local
 export const localhostSetting = {
   api: {
-    url: process.env.REACT_APP_LOCAL_API_URL,
-    mode: "cors",
+    BaseUrl: process.env.REACT_APP_API_BASE_URL,
+    mode: 'cors',
+    WebSocketUrl: process.env.REACT_APP_WEB_SOCKET_URL,
+    Occasion_url: process.env.REACT_APP_API_BASE_URL + "/admin-occasion",
   },
 };
